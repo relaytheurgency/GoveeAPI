fde3036344dd82dbc7f848fc0409d73398d12aed
BeauJBurroughs <Beau.j.burroughs@gmail.com>
2020-05-05 19:39:56 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,13 @@
 # format- "device-name": "device-address"
-devices = {
-
+devices = {"led":"00:11:22:33:44:55"
 }
+device_modes = ['set','on','off','strobe','diy']
 device_names = list(devices.keys())
+music_modes=['Energic','Spectrum','Rolling','Rhythm']
+scene_modes=['Sunrise','Sunset','Movie','Dating','Romantic','Blinking','Candlelight','Snowflake']
 addr_dev_dict = {v:k for k,v in devices.items()}
 handle = 21
 handle_hex = "0x{:04x}".format(handle)
+keepalive ="aa010000000000000000000000000000000000ab"
 server_secret = ""
 server_port = 5000
